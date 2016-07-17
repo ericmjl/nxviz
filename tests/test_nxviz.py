4b0c2ec769bd9cc912d279f5eef11d0a1ace4054
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-17 19:49:07 -0400

    moved assume_small_graph back into initialization test

--- tests/test_nxviz.py
+++ tests/test_nxviz.py
@@ -47,12 +47,8 @@ def test_correct_negative_angle(angle):
 @given(lists(integers(), unique=True),
        lists(integers(), unique=True, min_size=2, max_size=2))
 def test_initialization(nodes, edges):
+    assume(len(nodes) > 2 and len(nodes) < 5)
+    assume(len(edges) > 1 and len(edges) < 5)
     b = BasePlot(nodes, edges)
-    assume_small_graph(nodes, edges)
     assert b.nodecolors == ['blue'] * len(b.nodes)
     assert b.edgecolors == ['black'] * len(b.edges)
-
-
-def assume_small_graph(nodes, edges):
-    assume(len(nodes) > 2 and len(nodes) < 10)
-    assume(len(edges) > 1 and len(edges) < 10)
