4c4e0a575af2b95396ac4f0cb005c3f944b76596
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-16 19:09:03 -0500

    Added comments and attributes to __init__ function.

--- nxviz/base.py
+++ nxviz/base.py
@@ -11,13 +11,33 @@ class BasePlot(object):
         self.nodes = nodes
         self.edges = edges
 
+        # Set the node and edge props and colors.
+        # These are written with functions because there are type checks that
+        # have to take place first, and it would make the __init__ function
+        # unwieldy to have them all in here.
+        #
+        # Later if we support other backends, such as Bokeh, it may be wise to
+        # set these as "generic" parameters that we can write translators for
+        # to other packages.
         self.set_nodeprops(nodeprops)
         self.set_edgeprops(edgeprops)
         self.set_nodecolors(nodecolors)
         self.set_edgecolors(edgecolors)
+        # These functions end up setting the following object attributes:
+        # - self.nodeprops
+        # - self.edgeprops
+        # - self.nodecolors
+        # - self.edgecolors
 
+        # Initialize a figure object with an axes subplot.
+        # Later on if we support other backends, such as Bokeh, then it would
+        # be wise to take this out of the class.
         self.figure = plt.figure(figsize=figsize)
         self.ax = self.figure.add_subplot(1, 1, 1)
+
+        # We have an attribute that stores the x- and y-coordinates of each
+        # node. They should be in the same order as self.nodes.
+        self.node_coords = None
 
     def set_nodeprops(self, nodeprops):
         """
