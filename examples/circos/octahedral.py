a795951fa3d5054f08f0efdd4cf8af34bfd7a5ce
Eric J. Ma <ericmajinglong@gmail.com>
2016-07-17 21:21:10 -0400

    Updated octahedral example with matplotlib display and comments.

--- examples/circos/octahedral.py
+++ examples/circos/octahedral.py
@@ -1,6 +1,12 @@
+"""
+Displays a NetworkX octahedral graph to screen using a CircosPlot.
+"""
+
 from nxviz.circos import CircosPlot
 import networkx as nx
+import matplotlib.pyplot as plt
 
 G = nx.octahedral_graph()
 c = CircosPlot(G.nodes(), G.edges(), radius=5, node_radius=1)
 c.draw()
+plt.show()
