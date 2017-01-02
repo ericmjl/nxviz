138605dce791ebdc5f95c3deee53081fccda6a8a
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-02 16:54:04 -0500

    added tests for node order and grouping

--- tests/test_nxviz.py
+++ tests/test_nxviz.py
@@ -12,3 +12,45 @@ def test_initialization():
     b = BasePlot(graph=G)
 
     assert len(b.nodes) == len(G.nodes())
+
+def make_graph_for_grouping():
+
+    nodelist = [('Andrew', {'affiliation': 'MIT', 'year': 5}),
+                ('Felipe', {'affiliation': 'Broad', 'year': 4}),
+                ('Thomas', {'affiliation': 'Harvard', 'year': 2}),
+                ('Jerome', {'affiliation': 'Broad', 'year': 3}),
+                ('Lily', {'affiliation': 'MIT', 'year': 2}),
+                ('Tyrus', {'affiliation': 'MIT', 'year': 2})
+                ]
+
+    G = nx.Graph()
+    G.add_nodes_from(nodelist)
+    return G
+
+def test_init_group_nodes():
+    """
+    Tests initialization with grouping of nodes.
+
+    I provide a graph with nodes that should be grouped in a particular
+    fashion.
+    """
+
+    G = make_graph_for_grouping()
+    b = BasePlot(graph=G, node_grouping='affiliation')
+
+    assert b.nodes == sorted(G.nodes(data=True),
+                             key=lambda x: x[1]['affiliation'])
+
+
+def test_init_sort_nodes():
+    """
+    Tests initialization with sorting of nodes.
+
+    I provide a graph with nodes that should be sorted in a particular fashion.
+    """
+    G = make_graph_for_grouping()
+
+    b = BasePlot(graph=G, node_grouping='affiliation', node_order='year')
+
+    assert b.nodes == sorted(G.nodes(data=True),
+                             key=lambda x: (x[1]['affiliation'], x[1]['year']))
