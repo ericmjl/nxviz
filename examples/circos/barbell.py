094f1d7d07b863b6d273fc76fb997de2d59d5cf8
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-03 14:38:18 -0500

    added node_color and node_order example in barbell graph

--- examples/circos/barbell.py
+++ examples/circos/barbell.py
@@ -3,10 +3,13 @@ Displays a NetworkX barbell graph to screen using a CircosPlot.
 """
 
 from nxviz.plots import CircosPlot
+from random import choice
 import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.barbell_graph(m1=10, m2=3)
-c = CircosPlot(G)
+for n, d in G.nodes(data=True):
+    G.node[n]['class'] = choice(['one', 'two', 'three'])
+c = CircosPlot(G, node_color="class", node_order='class')
 c.draw()
 plt.show()
