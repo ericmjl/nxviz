a543c86e2d3c269960d7caa0a63d04bb5b39efe2
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-03 14:32:16 -0500

    added node_color diverging example to the lollipop graph

--- examples/circos/lollipop.py
+++ examples/circos/lollipop.py
@@ -3,10 +3,13 @@ Displays a NetworkX lollipop graph to screen using a CircosPlot.
 """
 
 from nxviz.plots import CircosPlot
+from random import choice
 import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.lollipop_graph(m=10, n=4)
-c = CircosPlot(G)
+for n, d in G.nodes(data=True):
+    G.node[n]['value'] = choice([-1, 0, 1])
+c = CircosPlot(G, node_color='value', node_grouping='value')
 c.draw()
 plt.show()
