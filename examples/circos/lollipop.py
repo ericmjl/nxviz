4bbae8b7bd8b5fdfedf5d841ef54a5c124d5ed11
Eric J. Ma <ericmajinglong@gmail.com>
2017-01-03 14:38:07 -0500

    updated values to be divergent

--- examples/circos/lollipop.py
+++ examples/circos/lollipop.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 G = nx.lollipop_graph(m=10, n=4)
 for n, d in G.nodes(data=True):
-    G.node[n]['value'] = choice([-1, 0, 1])
+    G.node[n]['value'] = choice([-1.0, 0.0, 1.0])
 c = CircosPlot(G, node_color='value', node_grouping='value')
 c.draw()
 plt.show()
