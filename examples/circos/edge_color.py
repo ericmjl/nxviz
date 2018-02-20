d4d9651fedbc5db8e5588287ed98fe37ff57d952
Nora <35599969+noragak@users.noreply.github.com>
2018-02-20 21:34:51 +0100

    Update edge_color.py

--- examples/circos/edge_color.py
+++ examples/circos/edge_color.py
@@ -11,21 +11,21 @@ nodelist = [('a'),
             ('d'),
             ('e'),
             ('f')
-           ]
+            ]
 edgelist1 = [('a', 'b', {'weight': 0.1}),
              ('a', 'c', {'weight': 0.2}),
              ('b', 'd', {'weight': 0.6}),
              ('c', 'd', {'weight': 0.7}),
              ('e', 'd', {'weight': 0.8}),
              ('e', 'f', {'weight': 1.0}),
-            ]
+             ]
 edgelist2 = [('a', 'b', {'class': 1}),
              ('a', 'c', {'class': 3}),
              ('b', 'd', {'class': 8}),
              ('c', 'd', {'class': 10}),
              ('e', 'd', {'class': 5}),
              ('e', 'f', {'class': 5}),
-            ]
+             ]
 
 G = nx.Graph()
 G.add_nodes_from(nodelist)
