ae15b7c7a069154fa414d3c710babecba28aafc5
Keelan <keelan.downton@gmail.com>
2018-06-17 22:51:37 -0400

    Refactor for 80-column lines

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,14 +6,14 @@ from random import choice, seed
 seed(123)
 n_nodes = 100
 
-headers = ['some_num', 'some_string', 'ten_things', 'color', 'group', 'location']
+headers = ['a_num', 'a_string', 'ten_things', 'color', 'group', 'location']
 data_generator = ((
     i,
     str(i),
     i % 10,
     choice(['red', 'green', 'blue']),
     choice(['A', 'B']),
-    choice(['New York', 'San Francisco', 'Chicago', 'LA', 'Austin', 'Vancouver'])
+    choice(['New York', 'San Francisco', 'Chicago', 'LA', 'Austin', 'Boston'])
 ) for i in range(0, n_nodes))
 
 test_dataframe = pd.DataFrame(data_generator, columns=headers)
@@ -50,16 +50,29 @@ def test_count_threshold():
 
 
 def test_node_properties():
-    g = graph_from_dataframe(test_dataframe, threshold_by_count_unique=3, node_property_columns=['ten_things'])
+    g = graph_from_dataframe(test_dataframe,
+                             threshold_by_count_unique=3,
+                             node_property_columns=['ten_things']
+                             )
 
     assert g.number_of_nodes() == 5
     assert g.number_of_edges() == 6
-    assert g.nodes['A'] == {'ten_things': '4|6|7|8|0|1|2|3|5|9', 'type': 'group'}
+    assert g.nodes['A'] == {
+        'ten_things': '4|6|7|8|0|1|2|3|5|9',
+        'type': 'group'
+    }
 
 
 def test_edge_properties():
-    g = graph_from_dataframe(test_dataframe, edge_property_columns=['ten_things', 'some_string'])
+    g = graph_from_dataframe(test_dataframe,
+                             edge_property_columns=['ten_things', 'a_string']
+                             )
 
     assert g.number_of_nodes() == 21
     assert g.number_of_edges() == 129
-    assert g['blue']['B'] == {'some_string': '96', 'ten_things': 6, 'type': 'color_group', 'weight': 20.0}
+    assert g['blue']['B'] == {
+        'a_string': '96',
+        'ten_things': 6,
+        'type': 'color_group',
+        'weight': 20.0
+    }
