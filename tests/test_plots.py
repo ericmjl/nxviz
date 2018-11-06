c93088efcdfa8c0ec5c8315f1f1388cc1eb4a1f0
brian cajes <brian@empiricotx.com>
2018-11-05 22:09:25 -0800

    init test image comparison

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -7,11 +7,19 @@ the following issue:
     https://github.com/ericmjl/nxviz/issues/160
 """
 
+import os
+
 from random import random
 
 import networkx as nx
 
 from nxviz import ArcPlot, CircosPlot, GeoPlot, MatrixPlot
+
+import matplotlib.pyplot as plt
+
+from matplotlib.testing.compare import compare_images
+
+from matplotlib.testing.decorators import _image_directories
 
 G = nx.erdos_renyi_graph(n=20, p=0.2)
 
@@ -21,9 +29,18 @@ for n, d in G_geo.nodes(data=True):
     G_geo.node[n]['longitude'] = random()
     G_geo.node[n]['dpcapacity'] = random()
 
+baseline_dir, result_dir = _image_directories(lambda: 'dummy func')
+
 
 def test_circos_plot():
     c = CircosPlot(G)  # noqa: F841
+    c.draw()
+    img_fn = "circos.png"
+    baseline_img_path = os.path.join(baseline_dir,img_fn)
+    result_img_path = os.path.join(result_dir, img_fn)
+    plt.savefig(result_img_path)
+    diff = compare_images(baseline_img_path, result_img_path, tol=0)
+    assert diff is None
 
 
 def test_matrix_plot():
