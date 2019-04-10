71d2f12829fd73ca58ba6873d84b7cf77bc4ab2e
Rabeez Riaz <19100165@lums.edu.pk>
2019-04-10 22:02:32 +0500

    Changed graph to be undirected in edge_color example

--- examples/arc/edge_color.py
+++ examples/arc/edge_color.py
@@ -8,19 +8,14 @@ from nxviz.plots import ArcPlot
 
 G = nx.DiGraph()
 
-NODES_EBUNCH = [
-    ("A", {"n_visitors": "1"}),
-    ("B", {"n_visitors": "3"}),
-    ("C", {"n_visitors": "4"}),
-]
-
-G.add_nodes_from(NODES_EBUNCH)
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
 
 
 G.add_edge("A", "B", weight=8, type="a")
 G.add_edge("A", "C", weight=8, type="b")
 G.add_edge("B", "C", weight=8, type="a")
-G.add_edge("C", "B", weight=8, type="b")
 
 
 edges = G.edges()
