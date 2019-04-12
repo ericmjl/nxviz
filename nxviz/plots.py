c4c7760c7f3bb31ff006a21ae319149088d6556c
Eric Ma <ericmjl@users.noreply.github.com>
2019-04-13 02:51:21 +0500

    Update nxviz/plots.py
    Co-Authored-By: Rabeez <19100165@lums.edu.pk>

--- nxviz/plots.py
+++ nxviz/plots.py
@@ -498,7 +498,7 @@ class CircosPlot(BasePlot):
         """Create the CircosPlot.
         """
 
-        # A CircosPlot only makes sense for atleast 3 nodes
+        # A CircosPlot only makes sense for at least 3 nodes
         assert len(graph.nodes) >= 3
         # Node labels are specified in the node_label_layout argument
         specified_layout = kwargs.pop("node_label_layout", None)
