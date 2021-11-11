b556ad090b44fd06cc81925a4f2a3c3c75de3572
Alireza <alirezatheh@gmail.com>
2021-11-11 04:08:27 +0330

    Fix circos node label layout examples

--- examples/circos/node_labels_numbers.py
+++ examples/circos/node_labels_numbers.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -5,11 +7,13 @@ import nxviz as nv
 from nxviz import annotate
 
 G = nx.erdos_renyi_graph(n=30, p=0.1)
+for n, d in G.nodes(data=True):
+    G.nodes[n]["group"] = randint(0, 5)
 G = nx.relabel_nodes(G, {i: "long name #" + str(i) for i in range(len(G))})
 
-nv.circos(G)
-# the rotated labels take up more space, so we will have to increase the
+nv.circos(G, group_by="group", node_color_by="group")
+annotate.circos_labels(G, group_by="group", layout="numbers")
+# The numbered labels take up more space, so we will have to increase the
 # padding a bit. 15% on all sides works well here.
 plt.tight_layout(rect=(0.15, 0.15, 0.85, 0.85))
-annotate.circos_labels(G, layout="numbers")
 plt.show()
--- examples/circos/node_labels_rotate.py
+++ examples/circos/node_labels_rotate.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import matplotlib.pyplot as plt
 import networkx as nx
 
@@ -5,11 +7,13 @@ import nxviz as nv
 from nxviz import annotate
 
 G = nx.erdos_renyi_graph(n=30, p=0.1)
+for n, d in G.nodes(data=True):
+    G.nodes[n]["group"] = randint(0, 5)
 G = nx.relabel_nodes(G, {i: "long name #" + str(i) for i in range(len(G))})
 
-c = nv.circos(G)
-annotate.circos_labels(G, layout="rotate")
-# the rotated labels take up more space, so we will have to increase the
-# padding a bit. 15% on all sides works well here.
-plt.tight_layout(rect=(0.15, 0.15, 0.85, 0.85))
+nv.circos(G, group_by="group", node_color_by="group")
+annotate.circos_labels(G, group_by="group", layout="rotate")
+# The rotated labels take up more space, so we will have to increase the
+# padding a bit. 5% on all sides works well here.
+plt.tight_layout(rect=(0.05, 0.05, 0.95, 0.95))
 plt.show()
