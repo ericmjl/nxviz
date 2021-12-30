1e0505e92895a32cb2d6b9399ea47d82c15e419d
Eric Ma <ericmjl@users.noreply.github.com>
2021-12-30 15:10:09 +0000

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as rqmts:
 
 setup(
     name="nxviz",
-    version="0.7.2",
+    version="0.7.3",
     description="Graph Visualization Package",
     long_description=readme,
     long_description_content_type="text/markdown",
