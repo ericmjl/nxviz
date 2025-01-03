cfde2d1cdfbaf8c144968327a8def12949cd0bb4
Zewen Kelvin Tuong <z.tuong@uq.edu.au>
2025-01-03 14:21:20 +1000

    bump version and add new examples

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt") as rqmts:
 
 setup(
     name="nxviz",
-    version="0.7.4",
+    version="0.7.5",
     description="Graph Visualization Package",
     long_description=readme,
     long_description_content_type="text/markdown",
