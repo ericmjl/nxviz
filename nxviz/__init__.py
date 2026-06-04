6de2138d78fd667a2585760e42ee7ef6b8b743d1
Eric Ma <ericmajinglong@gmail.com>
2026-06-04 08:25:16 -0400

    chore: remove deprecated API migration warning

--- nxviz/__init__.py
+++ nxviz/__init__.py
@@ -1,6 +1,5 @@
 """Top-level nxviz API."""
 
-import warnings
 from importlib.metadata import version
 
 from .api import (
@@ -29,17 +28,3 @@ __all__ = [
     "CircosPlot",
     "version",
 ]
-
-warnings.warn(
-    """
-nxviz has a new API! Version 0.7.4 onwards, the old class-based API is being
-deprecated in favour of a new API focused on advancing a grammar of network
-graphics. If your plotting code depends on the old API, please consider
-pinning nxviz at version 0.7.4, as the new API will break your old code.
-
-To check out the new API, please head over to the docs at
-https://ericmjl.github.io/nxviz/ to learn more. We hope you enjoy using it!
-
-(This deprecation message will go away in version 1.0.)
-"""
-)
